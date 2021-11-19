c3672887074d9655bfff73cd5c94a4623d9263a0
idahz99 <idahz99@gmail.com>
2021-11-20 02:31:16 +0800

    make it better

--- PKOB/settings.py
+++ PKOB/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ehne66naxmbu)axdoas6p6uivyf=6yi%tt484-lf5rj7090iqf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','Pkob.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','pkobweb.herokuapp.com']
 
 
 # Application definition
